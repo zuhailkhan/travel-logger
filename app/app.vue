243bd5a2d09bb7dc3f5c64fba41dd9d2c993b059
Zuhail Khan <60917698+zuhailkhan@users.noreply.github.com>
2025-09-22 06:38:57 +0530

    [TL-010] Add DaisyUI, Tailwindcss, Icons (#49)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+    <button class="btn btn-primary">
+      Default
+    </button>
+    <Icon name="tabler:current-location" />
+  </h1>
 </template>
