80814d1eb488426d11305174f59415722b823b9a
Zuhail Khan <zuhailahamedkhan@gmail.com>
2025-09-26 09:51:08 +0530

    [TL-013] Typesafe Env Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
+
+import "./app/lib/env";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
